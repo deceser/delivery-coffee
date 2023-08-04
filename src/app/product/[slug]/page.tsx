249df4397b3c81b42330e55233f75e1df8e4edf3
deceser <aufhaummtobsc@gmail.com>
2023-08-04 16:42:14 +0300

    fix [slug]

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,1 +1,9 @@
-// code will be here in the future
+import React from "react";
+
+type Props = {};
+
+const Coffee = (props: Props) => {
+  return; // code will be here in the future;
+};
+
+export default Coffee;
